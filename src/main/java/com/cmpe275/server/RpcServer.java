68373e5e532ed1f884417eb7699b7e077a75a021
Anav Sharma <anav.sharma@sjsu.edu>
2018-11-19 16:50:53 -0800

    adding the driver class for the Edge server.

---
+++ src/main/java/com/cmpe275/server/RpcServer.java
@@ -0,0 +1,58 @@
+package com.cmpe275.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class RpcServer {
+    private DataTransferServiceImpl dataTransferService;
+    private EdgeServer edgeServer;
+    private Server server;
+
+    RpcServer(String svrConf){
+        edgeServer = new EdgeServer(svrConf).getInstance(svrConf);
+        dataTransferService = new DataTransferServiceImpl(this.edgeServer);
+        server = ServerBuilder.forPort(edgeServer.getServerPort())
+                .addService(dataTransferService)
+                .build();
+    }
+
+    private void start(){
+        try {
+            server.start();
+            System.out.println("grpc server started");
+            server.awaitTermination();
+        }catch (IOException e){
+            System.err.println("IO excpetion with starting the grpc server!");
+            e.printStackTrace();
+            System.exit(1);
+        }catch (InterruptedException e){
+            System.err.println("Interrupt excpetion with awaiting termination of the grpc server!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    protected void stop() {
+        server.shutdown();
+    }
+
+    private void blockUntilShutdown() throws Exception {
+        server.awaitTermination();
+    }
+
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("Enter one of svr1, svr2, svr3 or svr4 as the command line arg to start.");
+        }
+        RpcServer myServer= new RpcServer(args[0]);
+        myServer.start();
+        try {
+            myServer.blockUntilShutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
